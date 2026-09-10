bb67c19556664aba1a50eab4486c7bfc642db0d6
TrueNeyzo <ostinfryman@gmail.com>
2026-09-10 14:46:22 +0300

    Update MainActivity.kt

--- app/src/main/java/com/neyzo/yamp/MainActivity.kt
+++ app/src/main/java/com/neyzo/yamp/MainActivity.kt
@@ -1,9 +1,7 @@
-\
 package com.neyzo.yamp
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.webkit.CookieManager
 import android.webkit.WebChromeClient
@@ -14,6 +12,7 @@ import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -38,36 +37,43 @@ class MainActivity : AppCompatActivity() {
         }
 
         webView.webChromeClient = WebChromeClient()
+
         webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
                 val uri = request?.url ?: return false
+
                 val scheme = uri.scheme?.lowercase()
+
                 if (scheme == "http" || scheme == "https") {
-                    // Keep API/CDN requests inside the page, but open actual navigations
-                    // (notably Yandex OAuth) in the system browser.
-                    startActivity(Intent(Intent.ACTION_VIEW, uri))
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            uri
+                        )
+                    )
                     return true
                 }
+
                 return false
             }
         }
 
         webView.loadUrl("file:///android_asset/index.html")
-
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (webView.canGoBack()) webView.goBack() else finish()
-            }
-        })
     }
 
-@Deprecated("Deprecated in Java")
-override fun onBackPressed() {
-    if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
-}
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
 
-override fun onDestroy() {
-
+    override fun onDestroy() {
         webView.stopLoading()
         webView.destroy()
         super.onDestroy()
